96ca84564bab714bf5bc6af618807ff40d349a12
kawai9126 <iroha.9126@gmail.com>
2021-07-12 19:23:27 +0000

    model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_095841) do
+ActiveRecord::Schema.define(version: 2021_07_12_191818) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "create_ad"
+    t.datetime "update_ad"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.string "contents", null: false
+    t.integer "non_price", null: false
+    t.string "image_id", null: false
+    t.boolean "is_sale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
